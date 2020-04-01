fe8745a4314c516417577312e5bee4e60d705551
unknown <ayushmishraims@gmail.com>
2020-04-01 18:17:45 +0530

    profile image upload

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+import TextField from '@material-ui/core/textfield';
 
+import classes from './Profile.module.scss';
 const Profile = () => {
-    return(
-        <div>Profile IS WORKING</div>
+    const style = {
+        width : '88%'
+    }
+    return (
+        <Modal>
+            
+            <form className = {classes.profileForm}> 
+            <div className = {classes.profileIcon}></div>
+            <TextField  label='Name' style = {style}/>
+            <TextField  label='Phone Number' type = 'number' style = {style}/>
+            <TextField  label='Address' type = 'text' style = {style}/>
+            <TextField  label='Profession' type = 'text' style = {style}/>
+            </form>
+        </Modal>
     )
 }
 
